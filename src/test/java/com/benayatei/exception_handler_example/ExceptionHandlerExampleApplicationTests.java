9e20c1f28c39a9f9e1761a6f803a8c855e30938e
Bahman <1benayatei@gmail.com>
2021-12-24 16:36:12 +0330

    Start project with spring 2.6.2

---
+++ src/test/java/com/benayatei/exception_handler_example/ExceptionHandlerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benayatei.exception_handler_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionHandlerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
